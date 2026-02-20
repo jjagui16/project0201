a5c485a9051308af6b61eec4bc251da348927d56
jjagui16 <joseaguilar16@my.lonestar.edu>
2026-02-19 19:42:42 -0600

    Project02-01 JS

---
+++ project02-01.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Jose Aguilar
+      Date:   02/19/2022
+
+      Filename: project02-01.js
+ */
+
