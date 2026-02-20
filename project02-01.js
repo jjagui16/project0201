4cefd396901abf2dfbcf8ccb10907f4d90eab430
jjagui16 <joseaguilar16@my.lonestar.edu>
2026-02-19 20:09:30 -0600

    added anonymous functions to evaluate temp conversions

--- project02-01.js
+++ project02-01.js
@@ -14,5 +14,14 @@ function FarenheitToCelsius (degree) {
 }
 
 function CelsiusToFarenheit (degree) {
-      return (degree + 32) * 1.8;
+      return (degree * 1.8) + 32;
+}
+
+document.getElementById("cValue").onchange = function() {
+      let cDegree = Number(document.getElementById("cValue").value);
+      document.getElementById("fValue").value = CelsiusToFarenheit(cDegree);
+}
+document.getElementById("fValue").onchange = function() {
+      let fDegree = Number(document.getElementById("fValue").value);
+      document.getElementById("cValue").value = FarenheitToCelsius(fDegree);
 }
